e438aa3c45f87ce131c3dd5bf391a951c6225dd9
irinascurtu <scurtuirina@gmail.com>
2020-08-24 21:50:17 +0300

    versioning- depracated and agnostic

---
+++ backend/src/Conference.Api/Conference.Api/Controllers/PingController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Conference.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiVersionNeutral]
+    [ApiController]
+    public class PingController : ControllerBase
+    {
+        // GET: api/
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "pong", "ping" };
+        }
+    }
+}
--- backend/src/Conference.Api/Conference.Api/Controllers/TalksController.cs
+++ backend/src/Conference.Api/Conference.Api/Controllers/TalksController.cs
@@ -14,7 +14,7 @@ namespace Conference.Api.Controllers
 {
     [Route("api/speakers/{speakerId}/talks")]
     [ApiController]
-    [ApiVersion("1.0")]
+    [ApiVersion("1.0", Deprecated = true)]
     public class TalksController : ControllerBase
     {
         private readonly ITalkRepository talkRepository;
@@ -30,7 +30,8 @@ namespace Conference.Api.Controllers
             this.mapper = mapper;
         }
 
-        [HttpGet(Name = "GetTalksForSpeaker")]
+        //[HttpGet(Name = "GetTalksForSpeaker")]
+        [HttpGet(Name = "GetTalksForSpeaker"), MapToApiVersion("2.0")]
         public ActionResult<IEnumerable<TalkV1Dto>> GetTalksForSpeaker(int speakerId)
         {
             if (!speakerRepository.SpeakerExists(speakerId))
